4342f7f6cbc5de224f9a1bd8fe59d81a9440942a
jn103508 <97132597+jn103508@users.noreply.github.com>
2022-01-13 12:48:40 -0500

    Update reducerFunctions.js
    Changed unshift to push to increase efficiency. Messages are in correct order when storing and updating.

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -14,7 +14,7 @@ export const addMessageToStore = (state, payload) => {
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
       const convoCopy = { ...convo, latestMessageText: { ...convo.latestMessageText }, messages: [ ...convo.messages ] };
-      convoCopy.messages.unshift(message);
+      convoCopy.messages.push(message);
       convoCopy.latestMessageText = message.text;
       return convoCopy;
     } else {
@@ -72,7 +72,7 @@ export const addNewConvoToStore = (state, recipientId, message) => {
     if (convo.otherUser.id === recipientId) {
       const convoCopy = { ...convo, id: { ...convo.id }, messages: [ ...convo.messages ], latestMessageText: { ...convo.latestMessageText } };
       convoCopy.id = message.conversationId;
-      convoCopy.messages.unshift(message);
+      convoCopy.messages.push(message);
       convoCopy.latestMessageText = message.text;
       return convoCopy;
     } else {
