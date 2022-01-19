76dbfe6edfdc7c7e750fb0df02ef89476f6e04f7
jn103508 <97132597+jn103508@users.noreply.github.com>
2022-01-19 17:04:45 -0500

    Update ImageBubble.js
    Used prop based styling instead on conditional class rendering and fixed some bugs

--- client/src/components/ActiveChat/ImageBubble.js
+++ client/src/components/ActiveChat/ImageBubble.js
@@ -3,27 +3,25 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Box, Dialog } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
-  senderImage: {
+  image: props => ({
+    margin: props.user === 'sender' 
+      ? "0 0 0 10px"
+      : "0 10px 0 0", 
     height: "200px",
-    borderRadius: "10px 10px 0 0",
-    marginLeft: "10px",
+    borderRadius: props.amount === 1 && props.text.length
+      ? "10px 10px 0px 0px"
+      : "10px 10px 0 10px",
     cursor: "pointer",
-  },
-  otherUserImage: {
-    height: "200px",
-    borderRadius: "10px 10px 0 0",
-    marginRight: "10px",
-    cursor: "pointer",
-  },
+  }),
   imageWithNoMessage: {
     height: "200px",
     borderRadius: "10px 10px 0 10px",
-  }
+  },
 }));
 
 const ImageBubble = (props) => {
-  const { attachments, text, user } = props;
-  const classes = useStyles();
+  const { attachments } = props;
+  const classes = useStyles(props);
 
   const [imageOpen, setImageOpen] = useState(false);
   const [clickedImage, setClickedImage] = useState("");
@@ -39,23 +37,18 @@ const ImageBubble = (props) => {
 
   return (
     <Box>
-      {attachments.length
-        ? attachments.map((attachment, index) => (
-            <img
-              key={index}
-              onClick={() => handleImageClick(attachment)}
-              className={
-                text
-                  ? user==="senderImage" ? `${classes.senderImage}`: `${classes.otherUserImage}`
-                  : `${classes.image} ${classes.imageWithNoMessage}`
-              }
-              src={attachment}
-              alt="senderImage"
-            />
-          ))
-        : ""}
+      {attachments.length > 0 &&
+        attachments.map((attachment, index) => (
+          <img
+            key={index}
+            onClick={() => handleImageClick(attachment)}
+            className={classes.image}
+            src={attachment}
+            alt="senderImage"
+          />
+        ))}
       <Dialog open={imageOpen} onClose={handleClose}>
-        <img src={clickedImage} alt="focusedImage"></img>
+        <img src={clickedImage} alt="focusedImage"/>
       </Dialog>
     </Box>
   );
