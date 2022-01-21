7622fb566c15e0a78585c6471170910b9225ef42
jn103508 <97132597+jn103508@users.noreply.github.com>
2022-01-21 13:19:51 -0500

    Update Image.js
    Added minus icon to let users delete selected images and formatted with prettier

--- client/src/components/ActiveChat/Image.js
+++ client/src/components/ActiveChat/Image.js
@@ -1,38 +1,48 @@
-import React from 'react';
+import React from "react";
 import { Container, Typography, Button } from "@material-ui/core";
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from "@material-ui/styles";
+import minusIcon from "../../images/minus-square-regular.svg";
 
 const useStyles = makeStyles(() => ({
-    imageSelectedContainer: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: "100%",
-        marginBottom: 10,
-        padding: 0
-    },
-    imageHeader: {
-        width: "92%",
-    },
-    deleteBtn: {
-        width: "8%",
-        color: "#cacaca",
-        fontSize: 16,
-        fontWeight: "200"
-    }
+  imageSelectedContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 10,
+    padding: 0,
+  },
+  imageHeader: {
+    width: "92%",
+  },
+  deleteBtn: {
+    width: "8%",
+    color: "#cacaca",
+  },
+  minusIcon: {
+    opacity: 0.15,
+    width: 24,
+  },
 }));
 
 const Image = (props) => {
-    const { onDelete, index, image } = props;
-    const classes= useStyles();
+  const { onDelete, index, image } = props;
+  const classes = useStyles();
 
-    return (
-        <Container className={classes.imageSelectedContainer}>
-            <Typography className={classes.imageHeader}>{image.name}</Typography>
-            <Button onClick={() => onDelete(index)} variant="text" className={classes.deleteBtn}>x</Button>
-        </Container>
-    )
-}
+  return (
+    <Container className={classes.imageSelectedContainer}>
+      <Typography className={classes.imageHeader}>{image.name}</Typography>
+
+      <Button
+        onClick={() => onDelete(index)}
+        variant="text"
+        className={classes.deleteBtn}
+      >
+        <img className={classes.minusIcon} src={minusIcon} alt="deleteIcon" />
+      </Button>
+    </Container>
+  );
+};
 
 export default Image;
